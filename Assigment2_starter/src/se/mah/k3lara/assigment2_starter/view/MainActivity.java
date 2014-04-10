0738fb3161e16a270cc4ad3daac9e6ec08166346
Anton Nyman <anton@antonnyman.se>
2014-04-10 14:18:23 +0200

    Added things for school

--- Assigment2_starter/src/se/mah/k3lara/assigment2_starter/view/MainActivity.java
+++ Assigment2_starter/src/se/mah/k3lara/assigment2_starter/view/MainActivity.java
@@ -3,7 +3,10 @@ package se.mah.k3lara.assigment2_starter.view;
 import se.mah.k3lara.assigment2_starter.R;
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,15 +14,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends Activity {
 
+	WebView webview;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		webview = new WebView(this);
+		webview.loadUrl("http://antonnyman.se");
 		setContentView(R.layout.activity_main);
-
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction().add(R.id.container, new StartFragment()).commit();
 		}
@@ -41,10 +48,28 @@ public class MainActivity extends Activity {
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
+		} else if(id == R.id.action_about) {
+			Dialog dialog = new Dialog(this);
+			dialog.setTitle("About");
+			dialog.setContentView(R.layout.fragment_about);
+			dialog.show();
+			
+//			FragmentTransaction ft = getFragmentManager().beginTransaction();
+//			AboutFragment aboutfragment = new AboutFragment();
+//			aboutfragment.show(ft, "fragment_about");
+		} else if(id == R.id.action_help) {
+			Dialog dialog = new Dialog(this);
+			dialog.setTitle("Help");
+			dialog.setContentView(webview);
+			dialog.show();
+			
+//			FragmentTransaction ft = getFragmentManager().beginTransaction();
+//			HelpFragment helpfragment = new HelpFragment();
+//			helpfragment.show(ft, "fragment_help");
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
+	
 	public void buttonClick(View v){
 		Log.i("k3lara","click");
 	}
--- Assigment2_starter/src/se/mah/k3lara/assigment2_starter/view/StartFragment.java
+++ Assigment2_starter/src/se/mah/k3lara/assigment2_starter/view/StartFragment.java
@@ -5,7 +5,6 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 public class StartFragment extends Fragment {
@@ -18,5 +17,7 @@ public class StartFragment extends Fragment {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 			return rootView;
-		}		
+		}
+		
+		
 }
